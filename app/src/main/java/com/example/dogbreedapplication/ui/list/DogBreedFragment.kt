5cc4c7d53a55f87f1f794675550b9c0b48f05e7c
morganhah99 <mjh58@njit.edu>
2024-05-03 09:54:03 -0400

    Fixed project structure

--- app/src/main/java/com/example/dogbreedapplication/ui/list/DogBreedFragment.kt
+++ app/src/main/java/com/example/dogbreedapplication/ui/list/DogBreedFragment.kt
@@ -1,4 +1,4 @@
-package com.example.dogbreedapplication.list
+package com.example.dogbreedapplication.ui.list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
--- app/src/main/java/com/example/dogbreedapplication/ui/list/DogBreedViewModel.kt
+++ app/src/main/java/com/example/dogbreedapplication/ui/list/DogBreedViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.dogbreedapplication.list
+package com.example.dogbreedapplication.ui.list
 
 import android.util.Log
 import androidx.lifecycle.LiveData
